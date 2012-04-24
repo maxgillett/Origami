ae2cde9124b44db1d9b5b8b3c47d8ae079fc8072
Zach Sherman <zksherm@gmail.com>
2012-04-24 01:00:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Origami::Application.config.secret_token = 'c34562edb6de4f77c1770f5257ffe5db23cd481ab9fceb45aa663b916a9006817fad973a82638ae3ddd7903c659b1ec3ce9e259688cad9294b955a9db033ab14'
